babf094ef28249cd3fac054a38ce3945d953b44f
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-07-30 23:27:19 +0200

    Add project files.

---
+++ prog6212-part-1-ST10249838/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace prog6212_part_1_ST10249838.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
